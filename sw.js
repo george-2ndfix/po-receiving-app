80ebf2d1d7ef6361fc47fd09cf4788125809fc17
george-2ndfix <george@2ndfix.com.au>
2026-05-11 14:13:13 +0930

    v95: bump SW cache to force browser update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'po-receiving-v94c'; // v86 allocate-from-awaiting: awaiting items now show Allocate from Stock button
+const CACHE_NAME = 'po-receiving-v95'; // v86 allocate-from-awaiting: awaiting items now show Allocate from Stock button
 const urlsToCache = [
   '/styles.css',
   '/storage-locations.json',
