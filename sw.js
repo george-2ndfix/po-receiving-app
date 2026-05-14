012eda1ad814202cfb98df35f132b2f4969942bb
george-2ndfix <george@2ndfix.com.au>
2026-05-14 23:16:02 +0000

    v112: bump SW cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'po-receiving-v108'; // v86 allocate-from-awaiting: awaiting items now show Allocate from Stock button
+const CACHE_NAME = 'po-receiving-v112'; // v86 allocate-from-awaiting: awaiting items now show Allocate from Stock button
 const urlsToCache = [
   '/styles.css',
   '/storage-locations.json',
